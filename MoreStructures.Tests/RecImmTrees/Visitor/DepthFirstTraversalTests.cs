696eb83508b119c5847a20c2ddec2a292281db7b
Antonio Aversa <differenziale@gmail.com>
2022-06-10 21:54:19 +0200

    Fully recursive depth first traversal of RecImmTrees (#40)

---
+++ MoreStructures.Tests/RecImmTrees/Visitor/DepthFirstTraversalTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoreStructures.RecImmTrees.Visitor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoreStructures.Tests.RecImmTrees.Visitor;
+
+[TestClass]
+public class DepthFirstTraversalTests
+{
+    [TestMethod]
+    public void Visit_TraversalOrderParentFirst()
+    {
+        var visits = new List<(int? edgeId, int nodeId)> { };
+        Visitor<TreeMock.Node, TreeTraversalContext<TreeMock.Edge, TreeMock.Node>> visitsAppenderVisitor
+            = (node, visitContext) => visits.Add((visitContext.IncomingEdge?.Id, node.Id));
+
+        var root = TreeMock.BuildExampleTree();
+        var visitStrategy = new FullyRecursiveDepthFirstTraversal<TreeMock.Edge, TreeMock.Node>()
+        {
+            TraversalOrder = TreeTraversalOrder.ParentFirst,
+            ChildrenSorter = edgesAndNodes => edgesAndNodes.OrderBy(edgeAndNode => edgeAndNode.Key.Id),
+        };
+        visitStrategy.Visit(root, visitsAppenderVisitor);
+        Assert.IsTrue(visits.SequenceEqual(new (int?, int)[]
+        {
+            (null, 0), (0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8),
+        }));
+    }
+
+    [TestMethod]
+    public void Visit_TraversalOrderChildrenFirst()
+    {
+        var visits = new List<(int? edgeId, int nodeId)> { };
+        Visitor<TreeMock.Node, TreeTraversalContext<TreeMock.Edge, TreeMock.Node>> visitsAppenderVisitor
+            = (node, visitContext) => visits.Add((visitContext.IncomingEdge?.Id, node.Id));
+
+        var root = TreeMock.BuildExampleTree();
+        var visitStrategy = new FullyRecursiveDepthFirstTraversal<TreeMock.Edge, TreeMock.Node>()
+        {
+            TraversalOrder = TreeTraversalOrder.ChildrenFirst,
+            ChildrenSorter = edgesAndNodes => edgesAndNodes.OrderBy(edgeAndNode => edgeAndNode.Key.Id),
+        };
+        visitStrategy.Visit(root, visitsAppenderVisitor);
+        Assert.IsTrue(visits.SequenceEqual(new (int?, int)[] 
+        { 
+            (0, 1), (3, 4), (4, 5), (2, 3), (5, 6), (6, 7), (1, 2), (7, 8), (null, 0),
+        }));
+    }
+
+    [TestMethod]
+    public void Visit_TraversalOrderNotSupported()
+    {
+        Visitor<TreeMock.Node, TreeTraversalContext<TreeMock.Edge, TreeMock.Node>> nopVisitor
+            = (node, visitContext) => { };
+        var visitStrategy = new FullyRecursiveDepthFirstTraversal<TreeMock.Edge, TreeMock.Node>()
+        {
+            TraversalOrder = (TreeTraversalOrder)(-1),
+            ChildrenSorter = edgesAndNodes => edgesAndNodes.OrderBy(edgeAndNode => edgeAndNode.Key.Id),
+        };
+
+        Assert.ThrowsException<NotSupportedException>(() => visitStrategy.Visit(new TreeMock.Node(0), nopVisitor));
+    }
+}
---
+++ MoreStructures/RecImmTrees/Visitor/TreeTraversalContext.cs
@@ -0,0 +1,16 @@
+﻿namespace MoreStructures.RecImmTrees.Visitor;
+
+/// <summary>
+/// The contextual information in a root-to-leaf traversal of a tree composed of 
+/// <see cref="IRecImmDictIndexedTreeNode{TEdge, TNode}"/> nodes and 
+/// <see cref="IRecImmDictIndexedTreeEdge{TEdge, TNode}"/> edges.
+/// </summary>
+/// <typeparam name="TEdge">The type of edges of the specific structure.</typeparam>
+/// <typeparam name="TNode">The type of nodes of the specific structure.</typeparam>
+/// <param name="ParentNode">The parent of the node being visited. Null when visiting the root node.</param>
+/// <param name="IncomingEdge">
+/// The edge outgoing from the parent, traversed to reach this node. Null when visiting the root node.
+/// </param>
+public record TreeTraversalContext<TEdge, TNode>(TNode? ParentNode, TEdge? IncomingEdge)
+    where TEdge : IRecImmDictIndexedTreeEdge<TEdge, TNode>
+    where TNode : IRecImmDictIndexedTreeNode<TEdge, TNode>;
