5226035c4450e2a06f6aaf07a34d93d6b8ec7fa1
Antonio Aversa <differenziale@gmail.com>
2022-06-12 23:40:52 +0200

    Shortest non-shared  substring: naive implementation

---
+++ MoreStructures.Tests/SuffixStructures/Matching/SNSSFinderTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoreStructures.SuffixStructures.Matching;
+using System;
+using System.Linq;
+
+namespace MoreStructures.Tests.SuffixTrees.Matching;
+
+public abstract class SNSSFinderTests
+{
+    protected ISNSSFinder Finder { get; }
+
+    protected SNSSFinderTests(ISNSSFinder finder)
+    {
+        Finder = finder;
+    }
+
+    [DataRow("AABBA", "AABBAAB", new string?[] { null })]
+    [DataRow("AABBA", "ABBAAB", new string?[] { "AABB" })]
+    [DataRow("AABBA", "BBAAB", new string?[] { "ABB" })]
+    [DataRow("BBAAA", "BBAA", new string?[] { "AAA" })]
+    [DataRow("AABBA", "BBAA", new string?[] { "AB" })]
+    [DataRow("BXAA", "AXBB", new string?[] { "BX", "XA", "AA" })]
+    [DataRow("BXZA", "AYBB", new string?[] { "X", "Z" })]
+    [DataRow("BXAA", "BXAA", new string?[] { null })]
+    [DataRow("AAAAAAAAAAAAAAAAAAAA", "BBBBBBBBBBBBBBB", new string?[] { "A" })]
+    [DataRow("AAAAAAAAAAAAAAAAAAAA", "BBBBBBBBBBBBBBBAA", new string?[] { "AAA" })]
+    [DataRow("AAAAAAAAAAAAAAAAAAAA", "BBBBBBBBBBBBBBBAAAA", new string?[] { "AAAAA" })]
+    [DataTestMethod]
+    public void Find_IsCorrect(string text1, string text2, string?[] expectedResults)
+    {
+        var result = Finder.Find(text1, text2);
+        Assert.IsTrue(expectedResults.Contains(result));
+    }
+
+}
---
+++ MoreStructures/SuffixStructures/Matching/ISNSSFinder.cs
@@ -0,0 +1,17 @@
+﻿namespace MoreStructures.SuffixStructures.Matching;
+
+/// <summary>
+/// Any algorithm finding the Shortest Non-shared Substring (SNSS) between two strings.
+/// </summary>
+public interface ISNSSFinder
+{
+    /// <summary>
+    /// Returns any substring of <paramref name="text1"/> which is not present in <paramref name="text2"/> and has 
+    /// minimal length. Returns <see langword="null"/> if there is no substring of <paramref name="text1"/> which is
+    /// not a substring of <paramref name="text2"/>, i.e. if the two string coincide.
+    /// </summary>
+    /// <param name="text1">The sequence of chars of the first text.</param>
+    /// <param name="text2">The sequence of chars of the second text.</param>
+    /// <returns>A string, containing the substring of <paramref name="text1"/>.</returns>
+    string? Find(IEnumerable<char> text1, IEnumerable<char> text2);
+}
