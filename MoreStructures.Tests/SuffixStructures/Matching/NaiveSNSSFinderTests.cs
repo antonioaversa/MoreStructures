5712c7de0ad4995fa864ce893e17a3c006896741
Antonio Aversa <differenziale@gmail.com>
2022-06-12 23:54:27 +0200

    Shortest non-shared  substring: naive implementation (#45)

---
+++ MoreStructures.Tests/SuffixStructures/Matching/NaiveSNSSFinderTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoreStructures.SuffixStructures.Matching;
+
+namespace MoreStructures.Tests.SuffixTrees.Matching;
+
+[TestClass]
+public class NaiveSNSSFinderTests : SNSSFinderTests
+{
+    public NaiveSNSSFinderTests()
+        : base(new NaiveSNSSFinder())
+    {
+    }
+}
