f0840f0d2744a5c50285392b2bebc791fb3b0825
Antonio Aversa <differenziale@gmail.com>
2022-06-13 12:27:03 +0200

    TreePath and ISuffixStructureEdge ContainsIndex and NonBiggerThan (#46)

--- MoreStructures/SuffixStructures/SuffixStructureEdgeExtensions.cs
+++ MoreStructures/SuffixStructures/SuffixStructureEdgeExtensions.cs
@@ -6,7 +6,8 @@
 public static class SuffixStructureEdgeExtensions
 {
     /// <summary>
-    /// Whether the first edge is in adjacency order w.r.t. the second edge.
+    /// Whether <paramref name="first"/> is in adjacency order w.r.t. <paramref name="second"/>, acoording to the 
+    /// provided <paramref name="order"/>.
     /// </summary>
     /// <param name="first">The edge to be compared for adjacency.</param>
     /// <param name="second">The edge to compare the first edge against.</param>
@@ -24,6 +25,52 @@ public static class SuffixStructureEdgeExtensions
         AdjacencyOrders order = AdjacencyOrders.BeforeOrAfter)
         where TEdge : ISuffixStructureEdge<TEdge, TNode>
         where TNode : ISuffixStructureNode<TEdge, TNode> =>
+
         (order.HasFlag(AdjacencyOrders.Before) && first.Start + first.Length == second.Start) ||
         (order.HasFlag(AdjacencyOrders.After) && second.Start + second.Length == first.Start);
+
+    /// <summary>
+    /// Whether the provided <paramref name="edge"/> contains the provided <paramref name="index"/>.
+    /// </summary>
+    /// <typeparam name="TEdge">
+    ///     <inheritdoc cref="ISuffixStructureEdge{TEdge, TNode}" path="/typeparam[@name='TEdge']"/>
+    /// </typeparam>
+    /// <typeparam name="TNode">
+    ///     <inheritdoc cref="ISuffixStructureNode{TEdge, TNode}" path="/typeparam[@name='TNode']"/>
+    /// </typeparam>
+    /// <param name="edge">The edge to check.</param>
+    /// <param name="index">The index of the char of the text, to look for. Must be non-negative.</param>
+    /// <returns>A boolean.</returns>
+    public static bool ContainsIndex<TEdge, TNode>(
+        this ISuffixStructureEdge<TEdge, TNode> edge,
+        int index)
+        where TEdge : ISuffixStructureEdge<TEdge, TNode>
+        where TNode : ISuffixStructureNode<TEdge, TNode> =>
+
+        index >= 0
+        ? edge.Start <= index && index < edge.Start + edge.Length
+        : throw new ArgumentOutOfRangeException(nameof(index), "Must be non-negative.");
+
+    /// <summary>
+    /// Whether the provided <paramref name="edge"/> has a <see cref="ISuffixStructureNode{TEdge, TNode}.Start"/> 
+    /// lower or equal than the provided <paramref name="index"/>.
+    /// </summary>
+    /// <typeparam name="TEdge">
+    ///     <inheritdoc cref="ISuffixStructureEdge{TEdge, TNode}" path="/typeparam[@name='TEdge']"/>
+    /// </typeparam>
+    /// <typeparam name="TNode">
+    ///     <inheritdoc cref="ISuffixStructureNode{TEdge, TNode}" path="/typeparam[@name='TNode']"/>
+    /// </typeparam>
+    /// <param name="edge">The edge to check.</param>
+    /// <param name="index">The index of the char of the text, to look for. Must be non-negative.</param>
+    /// <returns>A boolean.</returns>
+    public static bool ContainsIndexesNonBiggerThan<TEdge, TNode>(
+        this ISuffixStructureEdge<TEdge, TNode> edge,
+        int index)
+        where TEdge : ISuffixStructureEdge<TEdge, TNode>
+        where TNode : ISuffixStructureNode<TEdge, TNode> =>
+
+        index >= 0
+        ? edge.Start <= index
+        : throw new ArgumentOutOfRangeException(nameof(index), "Must be non-negative.");
 }
--- MoreStructures/SuffixStructures/SuffixStructureTreePathExtensions.cs
+++ MoreStructures/SuffixStructures/SuffixStructureTreePathExtensions.cs
@@ -13,19 +13,20 @@ public static class SuffixStructureTreePathExtensions
     /// <summary>
     /// Calculate the suffix corresponding to this path on the provided terminator-including text.
     /// </summary>
-    /// <param name="path">The path to traverse to build the suffix.</param>
-    /// <param name="text">The text, including the terminator character.</param>
     /// <typeparam name="TEdge">
     ///     <inheritdoc cref="ISuffixStructureEdge{TEdge, TNode}" path="/typeparam[@name='TEdge']"/>
     /// </typeparam>
     /// <typeparam name="TNode">
     ///     <inheritdoc cref="ISuffixStructureNode{TEdge, TNode}" path="/typeparam[@name='TNode']"/>
     /// </typeparam>
+    /// <param name="path">The path to traverse to build the suffix.</param>
+    /// <param name="text">The text, including the terminator character.</param>
     /// <returns>A <see cref="IValueEnumerable{T}"/> sequence of <see cref="char"/> containing the suffix.</returns>
     public static IValueEnumerable<char> SuffixFor<TEdge, TNode>(
         this TreePath<TEdge, TNode> path,
         TextWithTerminator text)
         where TEdge : TextWithTerminator.ISelector => 
+
         path.PathNodes
             .SelectMany(node => text[node.Key])
             .AsValue();
@@ -33,18 +34,65 @@ public static class SuffixStructureTreePathExtensions
     /// <summary>
     /// Whether this path identifies a suffix of the provided text.
     /// </summary>
-    /// <param name="path">The path, identifying a segment of the provided text.</param>
-    /// <param name="text">The text, including the terminator character.</param>
     /// <typeparam name="TEdge">
     ///     <inheritdoc cref="ISuffixStructureEdge{TEdge, TNode}" path="/typeparam[@name='TEdge']"/>
     /// </typeparam>
     /// <typeparam name="TNode">
     ///     <inheritdoc cref="ISuffixStructureNode{TEdge, TNode}" path="/typeparam[@name='TNode']"/>
     /// </typeparam>
+    /// <param name="path">The path, identifying a segment of the provided text.</param>
+    /// <param name="text">The text, including the terminator character.</param>
     /// <returns>True if the segment of text is also a suffix the text.</returns>
     public static bool IsSuffixOf<TEdge, TNode>
         (this TreePath<TEdge, TNode> path, 
         TextWithTerminator text)
         where TEdge : TextWithTerminator.ISelector =>
+
         text.EndsWith(path.SuffixFor(text));
+
+    /// <summary>
+    /// Whether the provided <paramref name="path"/> includes at least once, on any node of the path, the provided
+    /// <paramref name="index"/>.
+    /// </summary>
+    /// <typeparam name="TEdge">
+    ///     <inheritdoc cref="ISuffixStructureEdge{TEdge, TNode}" path="/typeparam[@name='TEdge']"/>
+    /// </typeparam>
+    /// <typeparam name="TNode">
+    ///     <inheritdoc cref="ISuffixStructureNode{TEdge, TNode}" path="/typeparam[@name='TNode']"/>
+    /// </typeparam>
+    /// <param name="path">The path to walk, looking for <paramref name="index"/>.</param>
+    /// <param name="index">The index of the char of the text, to look for. Must be non-negative.</param>
+    /// <returns>A boolean.</returns>
+    public static bool ContainsIndex<TEdge, TNode>(
+        this TreePath<TEdge, TNode> path,
+        int index)
+        where TEdge : ISuffixStructureEdge<TEdge, TNode>
+        where TNode : ISuffixStructureNode<TEdge, TNode> => 
+
+        index >= 0
+            ? path.PathNodes.Any(pathNode => pathNode.Key.ContainsIndex(index))
+            : throw new ArgumentOutOfRangeException(nameof(index), "Must be non-negative.");
+
+    /// <summary>
+    /// Whether the provided <paramref name="path"/> has at least a node starting at a index lower or equal than the 
+    /// provided <paramref name="index"/>.
+    /// </summary>
+    /// <typeparam name="TEdge">
+    ///     <inheritdoc cref="ISuffixStructureEdge{TEdge, TNode}" path="/typeparam[@name='TEdge']"/>
+    /// </typeparam>
+    /// <typeparam name="TNode">
+    ///     <inheritdoc cref="ISuffixStructureNode{TEdge, TNode}" path="/typeparam[@name='TNode']"/>
+    /// </typeparam>
+    /// <param name="path">The path to walk, looking for <paramref name="index"/>.</param>
+    /// <param name="index">The index of the char of the text, to look for. Must be non-negative.</param>
+    /// <returns>A boolean.</returns>
+    public static bool ContainsIndexesNonBiggerThan<TEdge, TNode>(
+        this TreePath<TEdge, TNode> path,
+        int index)
+        where TEdge : ISuffixStructureEdge<TEdge, TNode>
+        where TNode : ISuffixStructureNode<TEdge, TNode> =>
+
+        index >= 0
+            ? path.PathNodes.Any(pathNode => pathNode.Key.ContainsIndexesNonBiggerThan(index))
+            : throw new ArgumentOutOfRangeException(nameof(index), "Must be non-negative.");
 }
