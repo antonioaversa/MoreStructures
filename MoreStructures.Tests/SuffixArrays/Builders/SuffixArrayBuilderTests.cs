3a9d681f897333a22447dfaa3cacb70387f43ec3
Antonio Aversa <differenziale@gmail.com>
2022-06-25 16:15:51 +0200

    Suffix Array: naive builder (#74)

---
+++ MoreStructures.Tests/SuffixArrays/Builders/SuffixArrayBuilderTests.cs
@@ -0,0 +1,32 @@
+﻿using MoreStructures.SuffixArrays.Builders;
+using MoreStructures.SuffixStructures;
+
+namespace MoreStructures.Tests.SuffixArrays.Builders;
+
+[TestClass]
+public abstract class SuffixArrayBuilderTests<TEdge, TNode>
+    where TEdge : ISuffixStructureEdge<TEdge, TNode>
+    where TNode : ISuffixStructureNode<TEdge, TNode>
+{
+    protected Func<string, ISuffixArrayBuilder<TEdge, TNode>> BuilderBuilder { get; }
+
+    protected SuffixArrayBuilderTests(Func<string, ISuffixArrayBuilder<TEdge, TNode>> builderBuilder)
+    {
+        BuilderBuilder = builderBuilder;
+    }
+
+    [DataRow("", new int[] { 0 })]
+    [DataRow("ab", new int[] { 2, 0, 1 })]
+    [DataRow("ba", new int[] { 2, 1, 0 })]
+    [DataRow("aaa", new int[] { 3, 2, 1, 0 })]
+    [DataRow("ababcacaa", new int[] { 9, 8, 7, 0, 2, 5, 1, 3, 6, 4 })]
+    [DataRow("aabaacaaadba", new int[] { 12, 11, 6, 0, 3, 7, 1, 4, 8, 10, 2, 5, 9 })]
+    [DataTestMethod]
+    public void Build_IsCorrect(string text, int[] expectedResult)
+    {
+        var builder = BuilderBuilder(text);
+        var result = builder.Build();
+        Assert.IsTrue(expectedResult.SequenceEqual(result), 
+            $"Expected: [{string.Join(", ", expectedResult)}], Actual: [{string.Join(", ", result)}]");
+    }
+}
