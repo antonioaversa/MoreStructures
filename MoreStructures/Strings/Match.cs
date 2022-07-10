afdbd157555410acbe73a0c5b68bd74fd0e188c7
Antonio Aversa <differenziale@gmail.com>
2022-07-10 22:07:53 +0200

    Knuth-Morris-Pratt algorithm for text pattern matching

---
+++ MoreStructures/Strings/Match.cs
@@ -0,0 +1,13 @@
+﻿namespace MoreStructures.Strings;
+
+/// <summary>
+/// The result of matching a <see cref="string"/> text against a <see cref="string"/> pattern.
+/// </summary>
+/// <param name="Success">Whether the text has been completely matched or not.</param>
+/// <param name="Begin">The index in text of the best match.</param>
+/// <param name="MatchedChars">The number of chars matched from the text.</param>
+/// <param name="Path">The path of nodes visited by the matching algorithm.</param>
+/// <typeparam name="TPath">
+/// The type of path. Depends on the algorithm and the data structure used for pattern matching.
+/// </typeparam>
+public sealed record Match<TPath>(bool Success, int Begin, int MatchedChars, TPath Path);
