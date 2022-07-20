7fe62c631237a7c9df69944ff82646f9053f6671
Antonio Aversa <differenziale@gmail.com>
2022-07-20 23:44:53 +0200

    Double Length PCS Classifier: eq-classes-based implementation (#110)

---
+++ MoreStructures.Tests/SuffixArrays/CyclicShifts/EqClassesBasedDoubleLengthPcsClassifierTests.cs
@@ -0,0 +1,54 @@
+﻿using MoreStructures.SuffixArrays.CyclicShifts;
+
+namespace MoreStructures.Tests.SuffixArrays.CyclicShifts;
+
+[TestClass]
+public class EqClassesBasedDoubleLengthPcsClassifierTests : DoubleLengthPcsClassifierTests
+{
+    public EqClassesBasedDoubleLengthPcsClassifierTests()
+        : base(cbi => new EqClassesBasedDoubleLengthPcsClassifier(
+            cbi.PcsLength, cbi.EqClassesPcsHalfLength, cbi.Order))
+    {
+    }
+
+    [TestMethod]
+    public void Ctor_RaisesExceptionWhenPcsLengthIsNotCoherentWithEqClassesPcsHalfLength()
+    {
+        Assert.ThrowsException<ArgumentOutOfRangeException>(
+            () => new EqClassesBasedDoubleLengthPcsClassifier(-2, new[] { 0 }, new[] { 0 }));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(
+            () => new EqClassesBasedDoubleLengthPcsClassifier(0, new[] { 0 }, new[] { 0 }));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(
+            () => new EqClassesBasedDoubleLengthPcsClassifier(2, new[] { 0 }, new[] { 0 }));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(
+            () => new EqClassesBasedDoubleLengthPcsClassifier(2, Array.Empty<int>(), Array.Empty<int>()));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(
+            () => new EqClassesBasedDoubleLengthPcsClassifier(4, new[] { 0, 1, 2 }, new[] { 0, 1, 2 }));
+    }
+
+    [TestMethod]
+    public void Ctor_RaisesExceptionWhenOrderIsNotCoherentWithEqClassesPcsHalfLength()
+    {
+        Assert.ThrowsException<ArgumentException>(
+            () => new EqClassesBasedDoubleLengthPcsClassifier(2, new[] { 0, 1 }, new[] { 0 }));
+        Assert.ThrowsException<ArgumentException>(
+            () => new EqClassesBasedDoubleLengthPcsClassifier(2, new[] { 0, 1 }, new[] { 0, 1, 2 }));
+        Assert.ThrowsException<ArgumentException>(
+            () => new EqClassesBasedDoubleLengthPcsClassifier(2, new[] { 0, 1 }, Array.Empty<int>()));
+        Assert.ThrowsException<ArgumentException>(
+            () => new EqClassesBasedDoubleLengthPcsClassifier(4, new[] { 0, 1, 2, 3 }, new[] { 0, 1, 2, 3, 4 }));
+        Assert.ThrowsException<ArgumentException>(
+            () => new EqClassesBasedDoubleLengthPcsClassifier(4, new[] { 0, 1, 2, 3 }, new[] { 0, 1, 2 }));
+    }
+
+    [TestMethod]
+    public void Ctor_RaisesExceptionWhenPcsLengthIsNotEven()
+    {
+        Assert.ThrowsException<ArgumentException>(
+            () => new EqClassesBasedDoubleLengthPcsClassifier(1, new[] { 0, 1 }, new[] { 0, 1 }));
+        Assert.ThrowsException<ArgumentException>(
+            () => new EqClassesBasedDoubleLengthPcsClassifier(1, new[] { 0, 1 }, new[] { 0, 1 }));
+        Assert.ThrowsException<ArgumentException>(
+            () => new EqClassesBasedDoubleLengthPcsClassifier(3, new[] { 0, 1, 2, 4 }, new[] { 0, 1, 2, 4 }));
+    }
+}
