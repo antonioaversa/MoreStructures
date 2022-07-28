3d85b9ee0da2821a7c44f2ea3ef9c9646bffb968
Antonio Aversa <differenziale@gmail.com>
2022-07-28 11:01:30 +0200

    LCP between two strings: improve doc

--- MoreStructures/Utilities/StringUtilities.cs
+++ MoreStructures/Utilities/StringUtilities.cs
@@ -11,8 +11,31 @@ public static class StringUtilities
     /// <see cref="char"/>.
     /// </summary>
     /// <returns>
-    /// An integer betwen 0 and the length of the shortest of the enumerabbles provided.
+    /// An integer betwen 0 and the length of the shortest of the enumerables provided.
     /// </returns>
+    /// <remarks>
+    ///     <para id="algorithm">
+    ///     ALGORITHM
+    ///     <br/>
+    ///     - The algorithm iterates over two enumerators in parallel, one enumerator per enumerable provided.
+    ///       <br/>
+    ///     - It keeps running the enumerators until one of the two is over, or the current items of the two 
+    ///       enumerators are different.
+    ///       <br/>
+    ///     - It keeps a counter of the number of chars found equal from the beginning, which is returned as result.
+    ///     </para>
+    ///     <para id="complexity">
+    ///     COMPLEXITY
+    ///     <br/>
+    ///     - Enumerator creation, moving to next item and accessing the current item are all constant-time operations.
+    ///       <br/>
+    ///     - If the two enumerables have length l1 and l2 respectively, there are at most max(l1, l2) iterations.
+    ///       <br/>
+    ///     - The current item of each of the enumerable has a constant size.
+    ///       <br/>
+    ///     - If l1 and l2 are O(n), Time Complexity is O(n) and Space Complexity is O(1).
+    ///     </para>
+    /// </remarks>
     public static int LongestPrefixInCommon(IEnumerable<char> enumerable1, IEnumerable<char> enumerable2)
     {
         var result = 0;
