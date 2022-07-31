cd0554a1aec6e882f1ac514778618a3046e4b010
Antonio Aversa <differenziale@gmail.com>
2022-07-31 13:48:04 +0200

    Suffix Trees: equivalence against TextWithTerminator(s)

--- MoreStructures/SuffixTrees/SuffixTreeNode.cs
+++ MoreStructures/SuffixTrees/SuffixTreeNode.cs
@@ -39,14 +39,26 @@ public abstract record SuffixTreeNode(IDictionary<SuffixTreeEdge, SuffixTreeNode
     /// <summary>
     /// Builds an intermediate node, i.e. a node with children and their corresponding incoming edges.
     /// </summary>
-    public record Intermediate(IDictionary<SuffixTreeEdge, SuffixTreeNode> Children) 
-        : SuffixTreeNode(Children, null);
+    public record Intermediate(IDictionary<SuffixTreeEdge, SuffixTreeNode> Children)
+        : SuffixTreeNode(Children, null)
+    {
+        /// <inheritdoc/>
+        public override bool IsEquivalentTo(SuffixTreeNode other, params TextWithTerminator[] texts) =>
+            other is Intermediate(var otherChildren) &&
+            SuffixTreeNode.HaveEquivalentChildren(texts.GenerateFullText().fullText, Children, otherChildren);
+    }
 
     /// <summary>
     /// Builds a leaf, i.e. a node with no children and the start index of the suffix in the text.
     /// </summary>
-    public record Leaf(int LeafStart) 
-        : SuffixTreeNode(new Dictionary<SuffixTreeEdge, SuffixTreeNode> { }, LeafStart);
+    public record Leaf(int LeafStart)
+        : SuffixTreeNode(new Dictionary<SuffixTreeEdge, SuffixTreeNode> { }, LeafStart)
+    {
+        /// <inheritdoc/>
+        public override bool IsEquivalentTo(SuffixTreeNode other, params TextWithTerminator[] texts) =>
+            other is Leaf(var otherLeafStart) && 
+            LeafStart == otherLeafStart;
+    }
 
     /// <inheritdoc/>
     public IDictionary<SuffixTreeEdge, SuffixTreeNode> Children { get; } 
@@ -81,4 +93,68 @@ public abstract record SuffixTreeNode(IDictionary<SuffixTreeEdge, SuffixTreeNode
     /// Sealed to prevent compiler from superceding <see cref="ToString()"/> in derived record.
     /// </remarks>
     public override sealed string ToString() => Stringifier.Stringify(this);
+
+    /// <summary>
+    /// Determines whether this <see cref="SuffixTreeNode"/> structure is equivalent to the provided 
+    /// <paramref name="other"/> structure, w.r.t. the provided <paramref name="texts"/>.
+    /// </summary>
+    /// <param name="other">The other tree, to compare this tree against.</param>
+    /// <param name="texts">The text, based on which to evaluate the tree equivalence.</param>
+    /// <returns>True if the two trees are equivalent for the provided text, false otherwise.</returns>
+    /// <remarks>
+    ///     <para id="definition">
+    ///     The definition of <see cref="SuffixTreeNode"/> structures equivalence depends on (a list of)
+    ///     <see cref="TextWithTerminator"/>: two trees can be equivalent for some texts, and not equivalent
+    ///     for others. This is because of the <b>Edge Compression</b> technique used by
+    ///     <see cref="SuffixTreeEdge"/>, to store edges in O(1) space, instead of O(n).
+    ///     <br/>
+    ///     Moreover, equivalence is based on the equivalence of edges and of the specific type of node.
+    ///     <br/>
+    ///     - Two <see cref="SuffixTreeEdge"/> E1 and E2 are equivalent w.r.t. a <see cref="TextWithTerminator"/> T, 
+    ///       if the string identified by each edge is the same: <c>T[E1] == T[E2]</c>.
+    ///       <br/>
+    ///     - Two <see cref="Leaf"/> L1 and L2 are equivalent if their <see cref="Leaf.LeafStart"/> is the same:
+    ///       <c>L1.LeafStart == L2.LeafStart</c>.
+    ///       <br/>
+    ///     - Two <see cref="Intermediate"/> I1 and I2 are equivalent if there is a 1-to-1 correspondence between 
+    ///       children edges and child nodes pointed by such edges are equivalent: 
+    ///       <c>for each edge e1 of I1.Children there exists exactly one equivalent edge e2 of I2.Children and
+    ///       I1.Children[e1] is equivalent to I2.Children[e2] w.r.t. T</c>.
+    ///     </para>
+    ///     <para id="complexity">
+    ///     - Checking for edges equivalence is a O(n) operation, where n is the number of nodes in the tree, since it 
+    ///       requires decompressing the labels of each of the edges being compare and compare for equality the two 
+    ///       resulting O(n) strings.
+    ///       <br/>
+    ///     - Checking for leaves equivalence is a O(1) operation, since it only requires comparing 
+    ///       <see cref="Leaf.LeafStart"/>, which has constant size.
+    ///       <br/>
+    ///     - Checking for intermediate nodes equivalence is a O(n) operation, amortized cost over the total number of 
+    ///       edges in the tree, since there are O(n) edges in total in the tree, an edge is visited multiple times
+    ///       but always in the context of a single node, and the node equivalence check requires looking for the 
+    ///       equivalent edge.
+    ///       <br/>
+    ///     - In conclusion Time Complexity is O(n^2) and Space Complexity is O(n).
+    ///     </para>
+    /// </remarks>
+    public abstract bool IsEquivalentTo(SuffixTreeNode other, params TextWithTerminator[] texts);
+
+    private static bool HaveEquivalentChildren(
+        TextWithTerminator fullText,
+        IDictionary<SuffixTreeEdge, SuffixTreeNode> thisChildren,
+        IDictionary<SuffixTreeEdge, SuffixTreeNode> otherChildren)
+    {
+        foreach (var (childEdge, childNode) in thisChildren)
+        {
+            var correspondingEdgeAndNode = otherChildren
+                .Where(edgeAndNode => fullText[childEdge] == fullText[edgeAndNode.Key])
+                .ToList();
+            if (correspondingEdgeAndNode.Count != 1)
+                return false;
+            if (!childNode.IsEquivalentTo(correspondingEdgeAndNode[0].Value, fullText))
+                return false;
+        }
+
+        return true;
+    }
 }
