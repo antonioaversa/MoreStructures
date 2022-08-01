957f3e1fd0e793e4c4e083414bc08fe47c5cc6dd
Antonio Aversa <differenziale@gmail.com>
2022-08-01 22:25:15 +0200

    Suffix Tree Builder: Suffix and LCP Arrays based implementation (#123)

---
+++ MoreStructures.Tests/SuffixTrees/Builders/SuffixAndLcpArraysBasedSuffixTreeBuilderTests.cs
@@ -0,0 +1,20 @@
+﻿using MoreStructures.SuffixArrays.Builders;
+using MoreStructures.SuffixArrays.LongestCommonPrefix;
+using MoreStructures.SuffixTrees.Builders;
+
+namespace MoreStructures.Tests.SuffixTrees.Builders;
+
+[TestClass]
+public class SuffixAndLcpArraysBasedSuffixTreeBuilderTests : SuffixTreeBuilderTests
+{
+    public SuffixAndLcpArraysBasedSuffixTreeBuilderTests()
+        : base(new SuffixAndLcpArraysBasedSuffixTreeBuilder(
+            text =>
+            {
+                var suffixArray = new NaiveSuffixArrayBuilder(text).Build();
+                var lcpArray = new KasaiLcpArrayBuilder(text, suffixArray).Build();
+                return (suffixArray, lcpArray);
+            }))
+    {
+    }
+}
