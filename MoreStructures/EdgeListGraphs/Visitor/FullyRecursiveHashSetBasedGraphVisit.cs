d76fb6c15008cc7becf4118759e5954f3bf25d6b
Antonio Aversa <differenziale@gmail.com>
2022-08-11 15:37:29 +0200

    Edge List Graphs: HashSet-based recursive visit (#134)
    * Edge List Graphs: HashSet-based recursive visit
    
    * Reduce cyclomatic complexity of recursive method performing graph visit

---
+++ MoreStructures/EdgeListGraphs/Visitor/FullyRecursiveHashSetBasedGraphVisit.cs
@@ -0,0 +1,59 @@
+﻿namespace MoreStructures.EdgeListGraphs.Visitor;
+
+/// <summary>
+/// A <see cref="IVisitStrategy"/> implementation which uses an <see cref="HashSet{T}"/> of <see cref="int"/> to store
+/// already visited vertices, while visiting the graph, and performs the visit recursively.
+/// </summary>
+public class FullyRecursiveHashSetBasedGraphVisit : IVisitStrategy
+{
+    /// <summary>
+    /// Whether the provided <see cref="EdgeListGraph"/> should be considered as directed (i.e. edges should be 
+    /// traversed from start to end) or inderected (i.e. edges should be traversed both from start to end and from
+    /// end to start).
+    /// </summary>
+    public bool DirectedGraph { get; }
+
+    /// <summary>
+    ///     <inheritdoc cref="FullyRecursiveHashSetBasedGraphVisit"/>
+    /// </summary>
+    /// <param name="directedGraph">
+    ///     <inheritdoc cref="DirectedGraph" path="/summary"/>
+    /// </param>
+    public FullyRecursiveHashSetBasedGraphVisit(bool directedGraph)
+    {
+        DirectedGraph = directedGraph;
+    }
+
+    /// <inheritdoc/>
+    public IEnumerable<int> Visit(EdgeListGraph graph, int start)
+    {
+        var (_, edges) = graph;
+        var alreadyVisited = new HashSet<int>();
+
+        RExplore(edges, alreadyVisited, start);
+        return alreadyVisited;
+    }
+
+    private void RExplore(IList<(int start, int end)> edges, HashSet<int> alreadyVisited, int start)
+    {
+        alreadyVisited.Add(start);
+
+        var unexploredVertices = GetUnexploredAdjacentVertices(edges, alreadyVisited, start);
+
+        foreach (var unexploredVertex in unexploredVertices.Where(uv => uv.HasValue).Cast<int>())
+            RExplore(edges, alreadyVisited, unexploredVertex);
+    }
+
+    private IEnumerable<int?> GetUnexploredAdjacentVertices(
+        IList<(int start, int end)> edges, HashSet<int> alreadyVisited, int start)
+    {
+        Func<(int start, int end), int?> edgeStartCheck =
+            edge => edge.end == start && !alreadyVisited.Contains(edge.start) ? edge.start : null;
+        Func<(int start, int end), int?> edgeEndCheck =
+            edge => edge.start == start && !alreadyVisited.Contains(edge.end) ? edge.end : null;
+
+        return DirectedGraph
+            ? edges.Select(edge => edgeEndCheck(edge))
+            : edges.Select(edge => edgeEndCheck(edge) ?? edgeStartCheck(edge));
+    }
+}
---
+++ MoreStructures/EdgeListGraphs/Visitor/IVisitStrategy.cs
@@ -0,0 +1,24 @@
+﻿namespace MoreStructures.EdgeListGraphs.Visitor;
+
+/// <summary>
+/// An algorithm exploring <see cref="EdgeListGraph"/> instances.
+/// </summary>
+public interface IVisitStrategy
+{
+    /// <summary>
+    /// Explores the provided <paramref name="graph"/>, returning the list of vertices which are reachable from the 
+    /// vertex with id <paramref name="start"/>, along the edges of the graph.
+    /// </summary>
+    /// <param name="graph">
+    /// The <see cref="EdgeListGraph"/> to be explored.
+    /// Can be considered directed or undirected, depending on the actual exploration implementation.
+    /// </param>
+    /// <param name="start">
+    /// The <see cref="int"/> id identifying the vertex, from which the exploration has to be started.
+    /// Different starting points will result into different sequences (by items and/or order) of vertices.
+    /// </param>
+    /// <returns>
+    /// The sequence of vertices
+    /// </returns>
+    IEnumerable<int> Visit(EdgeListGraph graph, int start);
+}
