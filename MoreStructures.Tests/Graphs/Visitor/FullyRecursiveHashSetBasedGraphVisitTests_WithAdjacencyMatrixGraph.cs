24f6617e93a0269057d24b1ce6a265ef253a1890
Antonio Aversa <differenziale@gmail.com>
2022-08-13 22:52:28 +0200

    Adjacency Matrix Graphs: HashSet-based recursive visit (#137)

---
+++ MoreStructures.Tests/Graphs/Visitor/FullyRecursiveHashSetBasedGraphVisitTests_WithAdjacencyMatrixGraph.cs
@@ -0,0 +1,26 @@
+﻿using MoreStructures.AdjacencyMatrixGraphs;
+using MoreStructures.Graphs.Visitor;
+
+namespace MoreStructures.Tests.Graphs.Visitor;
+
+[TestClass]
+public class FullyRecursiveHashSetBasedGraphVisitTests_WithAdjacencyMatrixGraph : VisitStrategyTests
+{
+    public FullyRecursiveHashSetBasedGraphVisitTests_WithAdjacencyMatrixGraph() : base(
+        (numberOfVertices, edges) => new AdjacencyMatrixGraph(BuildMatrix(numberOfVertices, edges)),
+        directedGraph => new FullyRecursiveHashSetBasedGraphVisit(directedGraph))
+    {
+    }
+
+    private static bool[,] BuildMatrix(int numberOfVertices, IList<(int start, int end)> edges)
+    {
+        var matrix = new bool[numberOfVertices, numberOfVertices];
+
+        foreach (var (start, end) in edges)
+        {
+            matrix[start, end] = true;
+        }
+
+        return matrix;
+    }
+}
