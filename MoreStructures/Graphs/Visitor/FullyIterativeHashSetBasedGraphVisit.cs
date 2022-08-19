6e750b5bb970f7744720b2f4d321a2ba7d21d373
Antonio Aversa <differenziale@gmail.com>
2022-08-19 23:58:28 +0200

    Hash-based connected components: complexity analysis (#143)

--- MoreStructures/Graphs/Visitor/FullyIterativeHashSetBasedGraphVisit.cs
+++ MoreStructures/Graphs/Visitor/FullyIterativeHashSetBasedGraphVisit.cs
@@ -18,6 +18,44 @@ public class FullyIterativeHashSetBasedGraphVisit : DirectionableVisit
     }
 
     /// <inheritdoc path="//*[not(self::remarks)]"/>
+    /// <remarks>
+    ///     <para id="advantages">
+    ///     ADVANTAGES AND DISADVANTAGES
+    ///     <br/>
+    ///     Implemented fully recursively, so limited by stack depth and usable with graphs of a "reasonable" size.
+    ///     </para>
+    ///     <para id="algorithm">
+    ///     ALGORITHM
+    ///     <br/>
+    ///     - The total number of vertices of the graph is found via <see cref="IGraph.GetNumberOfVertices"/>.
+    ///       <br/>
+    ///     - If such number is v, vertices are identified by integers from 0 to v - 1.
+    ///       <br/>
+    ///     - Iterate over all vertices, visiting all other vertices reachable from the current vertex i, in the same 
+    ///       way visit is performed by <see cref="IVisitStrategy.Visit(IGraph, int)"/>.
+    ///       <br/>
+    ///     - All visits share the same <see cref="HashSet{T}"/> of visited vertices, so that, if a vertex has been
+    ///       visited before, directly or via another vertex, it's not visited again.
+    ///       <br/>
+    ///     - Every time a new vertex is visited, such vertex is yielded into the output sequence.
+    ///     </para>
+    ///     <para id="complexity">
+    ///     COMPLEXITY
+    ///     <br/>
+    ///     - The complexity of retrieving the total number of vertices depends on  
+    ///       <see cref="IGraph.GetNumberOfVertices"/>. While being specific to the <see cref="IGraph"/>
+    ///       implementation being used, all implementation provide O(1) runtime for such operation.
+    ///       <br/>
+    ///     - The <see cref="HashSet{T}"/> shared across all visits ensures that each vertex is visited at most once.
+    ///       <br/>
+    ///     - Because each vertex is visited at most once throughout the entire execution, edges are visited at most 
+    ///       once, when edge direction is taken into account during the visit, and twice, when it is not.
+    ///       <br/>
+    ///     - Therefore, Time Complexity is O(v * Ta + e) and Space Complexity is O(v * Sa + e), where 
+    ///       v is the number of vertices, e is the number of edges and Ta and Sa are the time and space cost of 
+    ///       retrieving the neighborhood of a given vertex.
+    ///     </para>
+    /// </remarks>
     public override IEnumerable<int> DepthFirstSearch(IGraph graph)
     {
         var stack = new Stack<int>();
