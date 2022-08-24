f33ad2736dab3a149bcffacf7640449ef2cadf6b
Antonio Aversa <differenziale@gmail.com>
2022-08-24 18:21:00 +0200

    Fix failing XQueue test

--- MoreStructures.Tests/XifoStructures/XQueueTests.cs
+++ MoreStructures.Tests/XifoStructures/XQueueTests.cs
@@ -10,14 +10,14 @@ public class XQueueTests : XifoStructureTests
     }
 
     [TestMethod]
-    public void PushAndPop_AreInStackOrder()
+    public void PushAndPop_AreInQueueOrder()
     {
         var xifo = XifoBuilder();
         xifo.Push(3);
         xifo.Push(5);
         xifo.Push(7);
-        Assert.AreEqual(7, xifo.Pop());
-        Assert.AreEqual(5, xifo.Pop());
         Assert.AreEqual(3, xifo.Pop());
+        Assert.AreEqual(5, xifo.Pop());
+        Assert.AreEqual(7, xifo.Pop());
     }
 }
