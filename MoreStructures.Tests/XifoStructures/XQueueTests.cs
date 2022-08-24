60102abe4817874a791e46a627910b7fb5d9baa7
Antonio Aversa <differenziale@gmail.com>
2022-08-24 18:15:31 +0200

    XIFO Structures: Stack and Queue adapter to common interface

---
+++ MoreStructures.Tests/XifoStructures/XQueueTests.cs
@@ -0,0 +1,23 @@
+﻿using MoreStructures.XifoStructures;
+
+namespace MoreStructures.Tests.XifoStructures;
+
+[TestClass]
+public class XQueueTests : XifoStructureTests
+{
+    public XQueueTests() : base(() => new XQueue<int>())
+    {
+    }
+
+    [TestMethod]
+    public void PushAndPop_AreInStackOrder()
+    {
+        var xifo = XifoBuilder();
+        xifo.Push(3);
+        xifo.Push(5);
+        xifo.Push(7);
+        Assert.AreEqual(7, xifo.Pop());
+        Assert.AreEqual(5, xifo.Pop());
+        Assert.AreEqual(3, xifo.Pop());
+    }
+}
---
+++ MoreStructures/XifoStructures/XQueue.cs
@@ -0,0 +1,29 @@
+﻿namespace MoreStructures.XifoStructures;
+
+/// <summary>
+/// A <see cref="IXifoStructure{T}"/> based on a <see cref="Queue{T}"/>.
+/// </summary>
+/// <typeparam name="T">
+/// The type of items of the data structure, and of the underlying <see cref="Queue{T}"/>.
+/// </typeparam>
+public class XQueue<T> : IXifoStructure<T>
+{
+    private readonly Queue<T> Underlying = new();
+
+    /// <summary>
+    /// Invokes <see cref="Queue{T}.Dequeue"/> on the underlying <see cref="Queue{T}"/>.
+    /// </summary>
+    /// <returns>An item of type <typeparamref name="T"/>.</returns>
+    public T Pop() => Underlying.Dequeue();
+
+    /// <summary>
+    /// Invokes <see cref="Queue{T}.Enqueue(T)"/> on the underlying <see cref="Queue{T}"/>.
+    /// </summary>
+    /// <param name="item">An item of type <typeparamref name="T"/>.</param>
+    public void Push(T item) => Underlying.Enqueue(item);
+
+    /// <summary>
+    /// Invokes <see cref="Queue{T}.Count"/> on the underlying <see cref="Queue{T}"/>.
+    /// </summary>
+    public int Count => Underlying.Count;
+}
