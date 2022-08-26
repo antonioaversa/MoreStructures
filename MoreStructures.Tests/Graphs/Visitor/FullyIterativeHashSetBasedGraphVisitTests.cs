44c4ea3bf436b2f39d2c33be36777e2d62860375
Antonio Aversa <differenziale@gmail.com>
2022-08-26 18:25:33 +0200

    Graphs: Visit tests refactor (#151)

---
+++ MoreStructures.Tests/Graphs/Visitor/FullyIterativeHashSetBasedGraphVisitTests.cs
@@ -0,0 +1,39 @@
+﻿using MoreStructures.AdjacencyListGraphs;
+using MoreStructures.AdjacencyMatrixGraphs;
+using MoreStructures.EdgeListGraphs;
+using MoreStructures.Graphs.Visitor;
+
+namespace MoreStructures.Tests.Graphs.Visitor;
+
+[TestClass]
+public class FullyIterativeHashSetBasedGraphVisitTests_WithEdgeListGraph : VisitStrategyTests
+{
+    public FullyIterativeHashSetBasedGraphVisitTests_WithEdgeListGraph() : base(
+        (numberOfVertices, edges) => new EdgeListGraph(numberOfVertices, edges),
+        directedGraph => new FullyIterativeHashSetBasedGraphVisit(directedGraph))
+    {
+    }
+}
+
+[TestClass]
+public class FullyRecursiveHashSetBasedGraphVisitTests_WithAdjacencyListGraph : VisitStrategyTests
+{
+    public FullyRecursiveHashSetBasedGraphVisitTests_WithAdjacencyListGraph() : base(
+        (numberOfVertices, edges) => new AdjacencyListGraph(
+            GraphTestUtils.BuildNeighborhoods(numberOfVertices, edges)),
+        directedGraph => new FullyRecursiveHashSetBasedGraphVisit(
+            directedGraph))
+    {
+    }
+}
+
+[TestClass]
+public class FullyRecursiveHashSetBasedGraphVisitTests_WithAdjacencyMatrixGraph : VisitStrategyTests
+{
+    public FullyRecursiveHashSetBasedGraphVisitTests_WithAdjacencyMatrixGraph() : base(
+        (numberOfVertices, edges) => new AdjacencyMatrixGraph(
+            GraphTestUtils.BuildMatrix(numberOfVertices, edges)),
+        directedGraph => new FullyRecursiveHashSetBasedGraphVisit(directedGraph))
+    {
+    }
+}
