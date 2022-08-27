ae1e84e7b2f4fd9c159bcdeaf925e9c800d2efdf
Antonio Aversa <differenziale@gmail.com>
2022-08-27 16:08:26 +0200

    Graphs: Topological Sort - any path sink-based implementation (#153)
    * Graphs: Topological Sort - any path sink-based implementation
    
    * Remove unnecessary condition to increase branch coverage

---
+++ MoreStructures.Tests/Graphs/Sorting/AnyPathToSinkBasedTopologicalSortTests.cs
@@ -0,0 +1,14 @@
+﻿using MoreStructures.EdgeListGraphs;
+using MoreStructures.Graphs.Sorting;
+
+namespace MoreStructures.Tests.Graphs.Sorting;
+
+[TestClass]
+public class AnyPathToSinkBasedTopologicalSortTests_WithEdgeListGraph : TopologicalSortTests
+{
+    public AnyPathToSinkBasedTopologicalSortTests_WithEdgeListGraph() : base(
+        (numberOfVertices, edges) => new EdgeListGraph(numberOfVertices, edges),
+        new AnyPathToSinkBasedTopologicalSort())
+    {
+    }
+}
--- MoreStructures/Graphs/Sorting/ITopologicalSort.cs
+++ MoreStructures/Graphs/Sorting/ITopologicalSort.cs
@@ -8,8 +8,9 @@
 ///     DEFINITION
 ///     <br/>
 ///     Given a DAG <c>G(V, E)</c>, where V is the set of vertices of G and E is the set of directed edges of G,
-///     the topological sort of G, indicated here as TS, is a <see cref="IList{T}"/> of <see cref="int"/>, such that
-///     <c>for all couples (v1, v2) of V^2, if there is a directed path from v1 to v2, then TS[v1] &lt; TS[v2]</c>. 
+///     the topological sort of G, indicated here as TS, is a <see cref="IList{T}"/> of the first non-negative |V| 
+///     <see cref="int"/> values, such that <c>for all couples (v1, v2) of V^2, if there is a directed path from v1 to 
+///     v2, then TS[v1] &lt; TS[v2]</c>. 
 ///     <br/>
 ///     The topological sort defines a <b>linear order</b> of the vertices V of the DAG G.
 ///     <br/>
@@ -20,6 +21,9 @@
 ///       <br/>
 ///     - an undirected graph doesn't make distinction between the two different traversal directions of an edge, 
 ///       making each edge a cycle of two edges (1 edge if it is a loop).
+///     <br/>
+///     The topological sort is in general not unique, i.e. multiple orders are possible, which satisfy the definition
+///     above. Look at the documentation of the <see cref="Sort(IGraph)"/> method for some examples.
 ///     </para>
 /// </remarks>
 public interface ITopologicalSort
@@ -29,11 +33,51 @@ public interface ITopologicalSort
     /// </summary>
     /// <param name="dag">The <see cref="IGraph"/> instance representing a DAG (Direct Acyclic Graph).</param>
     /// <returns>
-    /// A list of the first n non-negative distinct integers, where n is the number of vertices in 
-    /// <paramref name="dag"/>.
+    /// A list TS, of the first n non-negative distinct integers, where n is the number of vertices in 
+    /// <paramref name="dag"/>. TS[i] represents the position of the vertex i in the topological sort.
     /// </returns>
     /// <remarks>
     ///     <inheritdoc cref="ITopologicalSort"/>
     /// </remarks>
+    /// <example>
+    /// The followin graph:
+    /// <code>
+    ///  0 --> 1 &lt;==&gt; 3
+    /// | ^   ^     /
+    /// | |  /     /
+    /// | | /     /
+    /// v |/     /
+    ///  2 &lt;-----
+    /// </code>
+    /// is NOT a DAG and doesn't have any valid topological sort.
+    /// <br/>
+    /// <br/>
+    /// The followin graph:
+    /// <code>
+    ///  0 --> 1 --&lt; 3
+    ///  |          /
+    ///  |         /
+    ///  |        /
+    ///  v       /
+    ///  2 &lt;----
+    /// </code>
+    /// is a DAG and has a single valid topological sort order: <c>new[] { 0, 1, 3, 2 })</c>.
+    /// <br/>
+    /// <br/>
+    /// The followin graph:
+    /// <code>
+    ///  0 --> 1 --&lt; 3
+    ///  |
+    ///  |
+    ///  |
+    ///  v
+    ///  2
+    /// </code>
+    /// is a DAG and has two valid topological sort orders:
+    /// <br/>
+    /// - <c>new[] { 0, 1, 3, 2 })</c>;
+    ///   <br/>
+    /// - <c>new[] { 0, 2, 1, 3 })</c>.
+    /// </example>
     IList<int> Sort(IGraph dag);
 }
