e483a60021b989b381491aba1501b6c06e0b244b
Antonio Aversa <differenziale@gmail.com>
2022-08-30 14:20:01 +0200

    Graphs: single DFS sink-based topological sort (#157)

---
+++ MoreStructures.Tests/Graphs/Sorting/SingleDfsSinkBasedTopologicalSortTestt.cs
@@ -0,0 +1,15 @@
+﻿using MoreStructures.EdgeListGraphs;
+using MoreStructures.Graphs.Sorting;
+using MoreStructures.Graphs.Visitor;
+
+namespace MoreStructures.Tests.Graphs.Sorting;
+
+[TestClass]
+public class SingleDfsSinkBasedTopologicalSortTests_WithEdgeListGraph : TopologicalSortTests
+{
+    public SingleDfsSinkBasedTopologicalSortTests_WithEdgeListGraph() : base(
+        (numberOfVertices, edges) => new EdgeListGraph(numberOfVertices, edges),
+        new SingleDfsSinkBasedTopologicalSort(() => new FullyIterativeHashSetBasedGraphVisit(true)))
+    {
+    }
+}
