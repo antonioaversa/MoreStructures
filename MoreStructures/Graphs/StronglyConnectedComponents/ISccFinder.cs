c9494b43ba777226a1df4c345823a5e3060e2569
Antonio Aversa <differenziale@gmail.com>
2022-08-31 17:22:19 +0200

    Graphs SCC: improve doc

--- MoreStructures/Graphs/StronglyConnectedComponents/ISccFinder.cs
+++ MoreStructures/Graphs/StronglyConnectedComponents/ISccFinder.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MoreStructures.Graphs.StronglyConnectedComponents;
+﻿namespace MoreStructures.Graphs.StronglyConnectedComponents;
 
 /// <summary>
 /// An algorithm finding the Strongly Connected Components (SCC) of a <see cref="IGraph"/>.
 /// </summary>
+/// <remarks>
+///     <para id="definition">
+///     DEFINITION
+///     <br/>
+///     - A Strongly Connected Component (SCC) of a graph G is a set of vertices of G such that, taken any two vertices
+///       v1 and v2 of G, there is a directed path from v1 to v2 and a directed path from v2 to v1.
+///       <br/>
+///     - That is, every vertex in a SCC can reach and can be reached by any other vertex of the same SCC, whereas two
+///       vertices of two different SCC can be such that one is reachable from the other, but not viceversa.
+///       <br/>
+///     - A SCC is an extension specific to directed graphs of the concept of Connected Component, which instead only
+///       requires a path from v1 to v2 for CC[v1] to be the same of CC[v2].
+///       <br/>
+///     - While in an undirected graph a path from v1 to v2 implies a path from v2 to v1, that's not the case for 
+///       directed graphs. So bidirectionality is required.
+///     </para>
+/// </remarks>
 public interface ISccFinder
 {
     /// <summary>
