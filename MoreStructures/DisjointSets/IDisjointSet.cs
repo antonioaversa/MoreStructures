ca2253137d47dea714d5cce0decebd8d5c4c3762
Antonio Aversa <differenziale@gmail.com>
2022-09-04 01:22:23 +0200

    Disjoint Sets: Quick Find implementation

--- MoreStructures/DisjointSets/IDisjointSet.cs
+++ MoreStructures/DisjointSets/IDisjointSet.cs
@@ -4,38 +4,61 @@
 /// A data structure modelling a collection of sets of non-negative consecutive integer values 0..k-1, where set can be
 /// easily merged together and values can be easily checked for membership to the same set.
 /// </summary>
+/// <remarks>
+/// Disjoint sets are effective when dealing with equivalence relationships, i.e. relationships which are reflexive, 
+/// symmetric and transitive.
+/// <br/>
+/// It's not directly usable when relationships have direction (e.g. if A points to B, B doesn't necessarily points to 
+/// A).
+/// </remarks>
 public interface IDisjointSet
 {
     /// <summary>
     /// The number of integer values in the data structure.
     /// </summary>
+    /// <value>
+    /// A non-negative <see cref="int"/>.
+    /// </value>
     int ValuesCount { get; }
 
     /// <summary>
     /// The number of distinct sets, the data structure is made of.
     /// </summary>
+    /// <value>
+    /// A non-negative <see cref="int"/>, non-bigger than <see cref="ValuesCount"/>.
+    /// </value>
     int SetCount { get; }
 
     /// <summary>
     /// Establishes a "union" relashionship between the integer values <paramref name="first"/> and 
     /// <paramref name="second"/>, merging the sets of the two values into a single set.
     /// </summary>
-    /// <param name="first">The value of the first integer.</param>
-    /// <param name="second">The value of the second integer.</param>
+    /// <param name="first">
+    /// The value of the first integer. Must be non-negative and smaller than <see cref="ValuesCount"/>.
+    /// </param>
+    /// <param name="second">
+    /// The value of the second integer. Must be non-negative and smaller than <see cref="ValuesCount"/>.
+    /// </param>
     void Union(int first, int second);
 
     /// <summary>
     /// Whether the two provided integer values belong to the same set, or two disjoint sets.
     /// </summary>
-    /// <param name="first">The value of the first integer.</param>
-    /// <param name="second">The value of the second integer.</param>
+    /// <param name="first">
+    /// The value of the first integer. Must be non-negative and smaller than <see cref="ValuesCount"/>.
+    /// </param>
+    /// <param name="second">
+    /// The value of the second integer. Must be non-negative and smaller than <see cref="ValuesCount"/>.
+    /// </param>
     /// <returns><see langword="true"/> if the values belong to the same set, false otherwise.</returns>
     bool AreConnected(int first, int second);
 
     /// <summary>
     /// Returns the set identifier of the provided <paramref name="value"/>.
     /// </summary>
-    /// <param name="value">The value of the integer, to find the set of.</param>
+    /// <param name="value">
+    /// The value of the integer, to find the set of. Must be non-negative and smaller than <see cref="ValuesCount"/>.
+    /// </param>
     /// <returns>An <see cref="int"/> identifing the set, <paramref name="value"/> is member of.</returns>
     int Find(int value);
 }
---
+++ MoreStructures/DisjointSets/QuickFindDisjointSet.cs
@@ -0,0 +1,94 @@
+﻿namespace MoreStructures.DisjointSets;
+
+/// <summary>
+/// An <see cref="IDisjointSet"/> implementation based on a simple <see cref="List{T}"/> to store set ids of each of 
+/// the values stored in the Disjoint Set.
+/// </summary>
+public class QuickFindDisjointSet : IDisjointSet
+{
+    private int[] SetIds { get; }
+
+    /// <summary>
+    /// Builds a Disjoint Set structure containing <paramref name="valuesCount"/> disjoint values, each one into its 
+    /// own singleton set.
+    /// </summary>
+    /// <param name="valuesCount"><inheritdoc cref="ValuesCount" path="/summary"/></param>
+    public QuickFindDisjointSet(int valuesCount)
+    {
+        ValuesCount = valuesCount >= 0 
+            ? valuesCount 
+            : throw new ArgumentException("Must be non-negative.", nameof(valuesCount));
+        SetIds = new int[valuesCount];
+
+        for (var i = 0; i < ValuesCount; i++)
+            SetIds[i] = i;
+    }
+
+    /// <inheritdoc/>
+    /// <remarks>
+    /// Set at construction time, based on the constructor parameter.
+    /// <br/>
+    /// Time and Space Complexity are O(1).
+    /// </remarks>
+    public int ValuesCount { get; }
+
+    /// <inheritdoc/>
+    /// <remarks>
+    /// Obtained as the number of distinct values in the underlying list, which is scanned linearly.
+    /// <br/>
+    /// Time Complexity is O(n) and Space Complexity is O(1), where n is <see cref="ValuesCount"/>.
+    /// </remarks>
+    public int SetCount => SetIds.Distinct().Count();
+
+    /// <inheritdoc/>
+    /// <remarks>
+    /// Calculated in constant-time by comparing the set id of the first value with the set id of the second value.
+    /// <br/>
+    /// Time and Space Complexity are O(1).
+    /// </remarks>
+    public bool AreConnected(int first, int second)
+    {
+        if (first < 0 || first >= ValuesCount)
+            throw new ArgumentException(
+                $"Must be non-negative and smaller than {nameof(ValuesCount)}.", nameof(first));
+        if (second < 0 || second >= ValuesCount)
+            throw new ArgumentException(
+                $"Must be non-negative and smaller than {nameof(ValuesCount)}.", nameof(second));
+
+        return SetIds[first] == SetIds[second];
+    }
+
+    /// <inheritdoc/>
+    /// <remarks>
+    /// Calculated in constant-time by retrieving the set id at index <paramref name="value"/> in the underlying list.
+    /// </remarks>
+    public int Find(int value)
+    {
+        if (value < 0 || value >= ValuesCount)
+            throw new ArgumentException(
+                $"Must be non-negative and smaller than {nameof(ValuesCount)}.", nameof(value));
+
+        return SetIds[value];
+    }
+
+    /// <inheritdoc/>
+    /// <remarks>
+    /// Retrieves the set id A of <paramref name="first"/> and B of <paramref name="second"/>, via <see cref="Find"/>.
+    /// <br/>
+    /// Then replaces all occurrences of B with A, in the underlying list.
+    /// <br/>
+    /// Time Complexity is O(n) and Space Complexity is O(1).
+    /// </remarks>
+    public void Union(int first, int second)
+    {
+        var firstSetId = Find(first);
+        var secondSetId = Find(second);
+
+        if (firstSetId == secondSetId)
+            return;
+
+        for (var i = 0; i < ValuesCount; i++)
+            if (SetIds[i] == secondSetId)
+                SetIds[i] = firstSetId;
+    }
+}
