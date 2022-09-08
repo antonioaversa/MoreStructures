19efc2d4d3de924d4c92ed71c1f6bb3ad049428d
Antonio Aversa <differenziale@gmail.com>
2022-09-09 01:22:08 +0200

    Linked Lists: AsNode extension method (#179)

---
+++ MoreStructures/Utilities/LinkedListExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreStructures.Utilities;
+
+/// <summary>
+/// Extensions for <see cref="LinkedList{T}"/>.
+/// </summary>
+public static class LinkedListExtensions
+{
+    /// <summary>
+    /// Generates an <see cref="IEnumerable{T}"/> of the <see cref="LinkedListNode{T}"/> of the provided 
+    /// <paramref name="list"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of objects contained by <paramref name="list"/>.</typeparam>
+    /// <param name="list">The list, to iterate over.</param>
+    /// <returns>A sequence of <see cref="LinkedListNode{T}"/>.</returns>
+    public static IEnumerable<LinkedListNode<T>> AsNodes<T>(this LinkedList<T> list)
+    {
+        for (var node = list.First; node != null; node = node.Next)
+            yield return node;
+    }
+}
