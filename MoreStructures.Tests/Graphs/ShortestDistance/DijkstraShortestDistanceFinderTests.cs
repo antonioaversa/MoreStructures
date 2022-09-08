3042c721fe4e092ea513121602368440c3046587
Antonio Aversa <differenziale@gmail.com>
2022-09-08 20:25:13 +0200

    Graphs: shortest distance - Dijkstra implementation (#178)

---
+++ MoreStructures.Tests/Graphs/ShortestDistance/DijkstraShortestDistanceFinderTests.cs
@@ -0,0 +1,15 @@
+﻿using MoreStructures.Graphs;
+using MoreStructures.Graphs.ShortestDistance;
+
+namespace MoreStructures.Tests.Graphs.ShortestDistance;
+
+[TestClass]
+public class DijkstraShortestDistanceFinderTests : ShortestDistanceFinderTests
+{
+    public DijkstraShortestDistanceFinderTests()
+        : base(
+            (numberOfVertices, edges) => new EdgeListGraph(numberOfVertices, edges),
+            () => new DijkstraShortestDistanceFinder())
+    {
+    }
+}
