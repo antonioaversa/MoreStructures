d87282b6f15deadb90e23ccc7bc7a7abde15b343
Antonio Aversa <differenziale@gmail.com>
2022-09-15 23:37:09 +0200

    List Sorting Extensions: IsSorted (#197)

---
+++ MoreStructures.Tests/Lists/Sorting/ExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿using MoreStructures.Lists.Sorting;
+
+namespace MoreStructures.Tests.Lists.Sorting;
+
+[TestClass]
+public class ExtensionsTests
+{
+    [TestMethod]
+    public void IsSorted_WithComparable_IsCorrectWithNumbers()
+    {
+        Assert.IsTrue(Array.Empty<int>().IsSorted());
+        Assert.IsTrue(new List<int> { }.IsSorted());
+        Assert.IsTrue(new[] { 1 }.IsSorted());
+        Assert.IsFalse(new[] { 3, 2, 4, 5 }.IsSorted());
+        Assert.IsTrue(new[] { 1, 2, 4, 5 }.IsSorted());
+        Assert.IsTrue(new[] { 1.0, 1.0, 4.0, 5.0 }.IsSorted());
+        Assert.IsFalse(new[] { 1.0, -1.0 }.IsSorted());
+        Assert.IsTrue(new[] { -1.0, 1.0 }.IsSorted());
+        Assert.IsFalse(new[] { 1.0f, 1.0f, -4.0f, 5.0f }.IsSorted());
+        Assert.IsTrue(Enumerable.Range(100, 0).ToList().IsSorted());
+    }
+
+    [TestMethod]
+    public void IsSorted_WithComparable_IsCorrectWithBool()
+    {
+        Assert.IsTrue(new[] { false, false, true }.IsSorted());
+        Assert.IsFalse(new[] { true, false, true }.IsSorted());
+        Assert.IsTrue(new[] { true, true, true }.IsSorted());
+    }
+
+    [TestMethod]
+    public void IsSorted_WithComparable_IsCorrectWithChars()
+    {
+        Assert.IsTrue(new[] { 'a', 'a', 'b' }.IsSorted());
+        Assert.IsTrue(new[] { 'a', 'b', 'c' }.IsSorted());
+        Assert.IsTrue(new[] { 'A', 'a', 'b' }.IsSorted());
+        Assert.IsFalse(new[] { 'a', 'c', 'b' }.IsSorted());
+        Assert.IsFalse(new[] { 'a', 'c', 'A' }.IsSorted());
+    }
+
+    [TestMethod]
+    public void IsSorted_WithComparer_IsCorrect()
+    {
+        var invertedComparer = new InvertedComparer<int>();
+        Assert.IsTrue(new[] { 5, 3, -1 }.IsSorted(invertedComparer));
+        Assert.IsFalse(new[] { 1, 2, 3 }.IsSorted(invertedComparer));
+    }
+
+    private sealed class InvertedComparer<T> : IComparer<T>
+        where T : IComparable<T>
+    {
+        public int Compare(T? x, T? y)
+        {
+            if (x == null) return y == null ? 0 : -1;
+            if (y == null) return 1;
+            return -x.CompareTo(y);
+        }
+    }
+}
---
+++ MoreStructures/Lists/Sorting/Extensions.cs
@@ -0,0 +1,43 @@
+﻿namespace MoreStructures.Lists.Sorting;
+
+/// <summary>
+/// Extensions methods for <see cref="IList{T}"/>, in the context of list sorting.
+/// </summary>
+public static class Extensions
+{
+    /// <summary>
+    /// Verifies that the provided <paramref name="list"/> is sorted.
+    /// </summary>
+    /// <typeparam name="T">The type of items of <paramref name="list"/>.</typeparam>
+    /// <param name="list">The <see cref="IList{T}"/> to check.</param>
+    /// <returns>
+    /// <see langword="true"/> if <paramref name="list"/> is sorted, <see langword="false"/> otherwise.
+    /// </returns>
+    /// <remarks>
+    /// Zips the list with itself, shifted one item forward.
+    /// <br/>
+    /// Then checks whether each of the couples of the zip has a <see cref="ValueTuple{T, U}.Item1"/> non-bigger than 
+    /// <see cref="ValueTuple{T, U}.Item2"/>.
+    /// <br/>
+    /// Stops at the first encountered.
+    /// <br/>
+    /// Time Complexity is O(n), when fully enumerated and in the worst case. Space Complexity is O(1).
+    /// </remarks>
+    public static bool IsSorted<T>(this IList<T> list)
+        where T : IComparable<T> => 
+        list.Zip(list.Skip(1)).All(c => c.First.CompareTo(c.Second) <= 0);
+
+    /// <inheritdoc cref="IsSorted{T}(IList{T})" path="//*[not(self::summary or self::remarks)]"/>
+    /// <summary>
+    /// Verifies that the provided <paramref name="list"/> is sorted, using the provided <paramref name="comparer"/>.
+    /// </summary>
+    /// <param name="list">The <see cref="IList{T}"/> to check.</param>
+    /// <param name="comparer">
+    /// The <see cref="IComparer{T}"/> to be used to comparer <typeparamref name="T"/> instances.
+    /// </param>
+    /// <remarks>
+    ///     <inheritdoc cref="IsSorted{T}(IList{T})"/>
+    /// </remarks>
+    public static bool IsSorted<T>(this IList<T> list, IComparer<T> comparer) =>
+        list.Zip(list.Skip(1)).All(c => comparer.Compare(c.First, c.Second) <= 0);
+}
