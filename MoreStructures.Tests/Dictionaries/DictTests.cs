2a57321701a464c517b1ae825a267e5cb085eb4c
Antonio Aversa <differenziale@gmail.com>
2022-11-05 21:58:04 +0100

    Dictionaries: BSTs

---
+++ MoreStructures.Tests/Dictionaries/DictTests.cs
@@ -0,0 +1,18 @@
+﻿namespace MoreStructures.Tests.Dictionaries;
+
+public abstract class DictTests
+{
+    protected Func<IDictionary<int, int>> Builder { get; }
+
+    protected DictTests(Func<IDictionary<int, int>> builder)
+    {
+        Builder = builder;
+    }
+
+    [TestMethod]
+    public void Count_IsCorrect()
+    {
+        var dictionary = Builder();
+        dictionary.Count();
+    }
+}
